805b47297c9a7450986ff448cdc3985c72c22ff2
CoderHaste <prateek.manjunath27@gmail.com>
2024-11-30 21:04:50 +0530

    Update NumberGuessing.c

--- NumberGuessing.c
+++ NumberGuessing.c
@@ -1,8 +1,8 @@
-#include<stdio.h> 
-#include<time.h> 
-#include<stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main(){
+int main() {
     int MAX;
     int MIN;
     int guesses = 0;
@@ -10,35 +10,37 @@ int main(){
     int answer;
 
     srand(time(0));
+    do {
+        printf("Enter Lower limit: ");
+        scanf("%d", &MIN);
+        printf("Enter Upper limit: ");
+        scanf("%d", &MAX);
 
-    printf("Enter Lower limit: ");
-    scanf("%d", &MIN);
-    printf("Enter Upper limit: ");
-    scanf("%d", &MAX);
+        if (MIN > MAX) {
+            printf("Invalid limits! Lower limit cannot be greater than Upper limit. Please try again.\n");
+        }
+    } while (MIN > MAX);
 
-    answer = (rand() % MAX) + MIN;
+    answer = (rand() % (MAX - MIN + 1)) + MIN;
 
-    do
-    {
+    do {
         printf("Enter your guess: ");
         scanf("%d", &guess);
-        if (guess > answer)
-        {
+
+        if (guess > answer) {
             printf("Guess was too high!\n");
-        }
-        else if (guess < answer)
-        {
+        } else if (guess < answer) {
             printf("Guess was too low!\n");
-        }
-        else {
+        } else {
             printf("CORRECT!\n");
         }
-        guesses++;     
+        guesses++;
     } while (guess != answer);
 
     printf("********************\n");
     printf("Answer: %d\n", answer);
     printf("Guesses: %d\n", guesses);
-    printf("********************");
+    printf("********************\n");
+
     return 0;
 }
